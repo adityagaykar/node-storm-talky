48927bf8cebd5f4663c233bb74908fb4f101ac63
Aditya Gaykar <adityagaykar@gmail.com>
2017-04-13 00:45:58 +0530

    updated dockerfile

--- docker_builds/classifier/web2py/applications/api/models/db.py
+++ docker_builds/classifier/web2py/applications/api/models/db.py
@@ -25,6 +25,9 @@ from gluon.contrib.appconfig import AppConfig
 # -------------------------------------------------------------------------
 myconf = AppConfig(reload=True)
 
+from gluon.custom_import import track_changes
+track_changes(True)
+
 if not request.env.web2py_runtime_gae:
     # ---------------------------------------------------------------------
     # if NOT running on Google App Engine use SQLite or other DB
